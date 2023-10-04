337a38249ee6075543f7c39c7e840d46e52265b1
jhin1m <ducanhfake4@gmail.com>
2023-10-04 14:54:47 +0200

    First commit

---
+++ app/Scrapers/MangaWeb.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Scrapers;
+
+use Symfony\Component\DomCrawler\Crawler;
+use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client;
+use App\Models\Comic;
+use Carbon\Carbon;
+
+class MangaWeb extends Scraper
+{
+   
+}
